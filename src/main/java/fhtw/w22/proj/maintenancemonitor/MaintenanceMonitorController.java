38a097f3dfbd3eddce62add4c2cbec659fb1c69b
yukarisoprano <yukarisoprano717@gmail.com>
2023-01-01 18:41:23 +0100

    Program Code Corrected

--- src/main/java/fhtw/w22/proj/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/fhtw/w22/proj/maintenancemonitor/MaintenanceMonitorController.java
@@ -14,42 +14,52 @@ import java.util.Scanner;
 public class MaintenanceMonitorController {
   @GetMapping("/api/message")
   String setMsg (Model model) throws FileNotFoundException {
-    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
-    LocalDateTime now = LocalDateTime.now();
     File f = new File ("api_stored.text");
     if(f.exists()){
       Scanner sc = new Scanner(f);
       if(!sc.hasNextLine()) {
         model.addAttribute("outputText", "Everything works as expected");
-        model.addAttribute("updateTime", "last update: " + dtf.format(now));
+        showUpdateTime(model);
 
       } else {
         while (sc.hasNextLine()){
           model.addAttribute("outputText", sc.nextLine());
         }
         sc.close();
+        showUpdateTime(model);
       }
     }
     else {
       model.addAttribute("outputText", "Everything works as expected");
-      model.addAttribute("updateTime", "last update: " + dtf.format(now));
+      showUpdateTime(model);
     }
     return "greenMaintenanceMonitor";
   }
   @GetMapping("api/message/set")
-  String setMsg (@RequestParam String m, Model model){
+  String setMsg (@RequestParam String m, Model model) throws FileNotFoundException {
+    // to set up the time string
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
     LocalDateTime now = LocalDateTime.now();
+    String updatedTime = "";
+
     // if the RequestParam is not empty, add the text in the api_stored.text
     if (!m.equals("")) {
       model.addAttribute ("outputText","OK");
-      model.addAttribute("updateTime", "last update: " + dtf.format(now));
+
+      // the updated time should be shown in the monitor
+      updatedTime = dtf.format(now);
+      model.addAttribute("updateTime", "last update: " + updatedTime);
+
       try {
-
+        // as we have no data bank, the message data and the update time data is saved in separate files.
         FileWriter fw = new FileWriter ("api_stored.text");
+        FileWriter fw_date = new FileWriter("api_update.text");
 
-        fw.write(m + " (last update: " + dtf.format(now) + ")");
+        fw.write(m);
+        fw_date.write ("last update: " + updatedTime);
+
         fw.close();
+        fw_date.close();
 
       } catch (IOException e){
         e.printStackTrace();
@@ -58,23 +68,50 @@ public class MaintenanceMonitorController {
 
     } else {
       model.addAttribute("outputText", "Everything works as expected");
-      model.addAttribute("updateTime", "last update: " + dtf.format(now));
+      showUpdateTime(model);
 
     }
     return "greenMaintenanceMonitor";
   }
   @GetMapping("api/message/reset")
-  String resetMsg(Model model){
+  String resetMsg(Model model) throws IOException {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
     LocalDateTime now = LocalDateTime.now();
+    String updatedTime = "";
 
     File f= new File ("api_stored.text");
     if (f.exists()){
+      updatedTime = dtf.format(now);
+      model.addAttribute("updateTime", "last update: " + updatedTime);
+      FileWriter fw_date = new FileWriter("api_update.text");
+      fw_date.write ("last update: " + updatedTime);
+      fw_date.close();
+
       f.delete();
     }
     model.addAttribute ("outputText", "OK");
-    model.addAttribute("updateTime", "last update: " + dtf.format(now));
+
+    showUpdateTime(model);
 
     return "greenMaintenanceMonitor";
+  }
+
+  /**
+   * @param model
+   * @throws FileNotFoundException
+   * This function is to show the last updated time (if it is not updated, will return the message "Not updated yet".
+   * Last updated time is saved in the file "api_update.text"
+   */
+  void showUpdateTime (Model model) throws FileNotFoundException {
+    File data_time = new File("api_update.text");
+    if (!data_time.exists()) {
+      model.addAttribute("updateTime", "Not updated yet");
+    } else {
+      Scanner sc_time = new Scanner(data_time);
+      while (sc_time.hasNextLine()) {
+        model.addAttribute("updateTime", sc_time.nextLine());
+      }
+      sc_time.close();
+    }
   }
 }
